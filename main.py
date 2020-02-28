789119ccf390b48dbaec93844e3efa35fc435b9b
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-02-28 18:29:22 +0100

    Fully functionnal terminal version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ who match an existing MySQL account.
 import argparse
 import getpass
 
-from program import ProgramTerminal
+from program import ProgramGraphic, ProgramTerminal
 from auth import Auth
 
 
@@ -18,10 +18,14 @@ def main():
     parser = argparse.ArgumentParser()
     my_auth = Auth()
     parser.add_argument("user", help="is the user for the database")
+    parser.add_argument("-t", "--terminal", help="Launches the program in terminal mode", action="store_true")
     args = parser.parse_args()
     my_auth.user = args.user
     my_auth.password = getpass.getpass()
-    pt = ProgramTerminal(my_auth)
-    pt.launch()
+    if args.terminal:
+        pr = ProgramTerminal(my_auth)
+    else:
+        pr = ProgramTerminal(my_auth)
+    pr.launch()
 
 main();

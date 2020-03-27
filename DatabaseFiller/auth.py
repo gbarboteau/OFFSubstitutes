ffdf4fb90a19d1cd22684f342dff82e6f553ea1d
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-03-27 17:28:45 +0100

    Typos

--- DatabaseFiller/auth.py
+++ DatabaseFiller/auth.py
@@ -4,9 +4,9 @@ a MySQL's already created profile
 (see the README for more info)
 """
 class Auth:
-	"""A token identifying the current user"""
+    """A token identifying the current user"""
     def __init__(self):
-    	"""The user is identified with its username 
+        """The user is identified with its username 
         and password (must match the database)
         """
         self.user = ""
